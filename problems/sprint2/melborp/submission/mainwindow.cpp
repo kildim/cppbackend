ea9b65a962f781c37070c7b12571cb0501779a00
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2024-12-14 19:37:07 +0300

    feat: Exercise sp2-9-4_melborp done

--- problems/sprint2/melborp/submission/mainwindow.cpp
+++ problems/sprint2/melborp/submission/mainwindow.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 
 #include "ui_mainwindow.h"
+QString Melborp(const QString& input) {
+    QString result = "";
+    for (QChar symbol : input) result = symbol + result;
+    return result;
+}
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
@@ -11,3 +16,18 @@ MainWindow::MainWindow(QWidget* parent)
 MainWindow::~MainWindow() {
     delete ui;
 }
+
+void MainWindow::on_pb_do_clicked()
+{
+    QString
+        revert_string,
+        string;
+
+    string
+        = this->ui->le_input->text();
+    revert_string
+        = Melborp(string);
+
+    this->ui->le_input->setText(revert_string);
+}
+
--- problems/sprint2/melborp/submission/mainwindow.h
+++ problems/sprint2/melborp/submission/mainwindow.h
@@ -16,6 +16,9 @@ public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pb_do_clicked();
+
 private:
     Ui::MainWindow* ui;
 };
