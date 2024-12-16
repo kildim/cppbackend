37664f7926cc8ce9047791ffdb123653de8c1429
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2024-12-16 20:55:03 +0300

    feat: s2-12-4-2 photoalbum-3 shows picture from resource file using index of the file in the directory

--- problems/sprint2/photoalbum-3/submission/mainwindow.cpp
+++ problems/sprint2/photoalbum-3/submission/mainwindow.cpp
@@ -1,2 +1,96 @@
 // Возьмите решение предыдущей задачи
 // о фотоальбоме в качестве заготовки.
+
+// Решение предыдущей задачи
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+const QString IMAGE_PATH = ":/cats/images/cat1.jpg";
+
+
+// Функция подгоняет изображение под нужный размер окна.
+QPixmap ResizeImgToFit(const QPixmap &src, int window_width, int window_height) {
+    int img_w = src.width();
+    int img_h = src.height();
+
+    double w_ratio = double(img_w) / window_width;
+    double h_ratio = double(img_h) / window_height;
+
+
+
+    if ( w_ratio < h_ratio ) {
+        return src.scaledToWidth(window_width);
+    } else {
+        return src.scaledToHeight(window_height);
+    }
+
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    SetFolder(":/cats/images/");
+    //SetPixmap(IMAGE_PATH /* подставьте сюда путь до ресурса */);
+    FitImage();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::FitImage()
+{
+    QPixmap
+        label_pixmap;
+    int
+        window_width = this->width(),
+        window_height = this->height();
+
+    Q_ASSERT(!active_pixmap.isNull());
+
+
+    label_pixmap = ResizeImgToFit(active_pixmap, window_width, window_height);
+
+    lbl_new_.setPixmap(label_pixmap);
+    lbl_new_.resize(label_pixmap.width(), label_pixmap.height());
+
+    int lbl_x = (window_width - lbl_new_.width()) / 2; // Координата x.
+    int lbl_y = (window_height - lbl_new_.height()) / 2; // Координата y.
+    lbl_new_.move(lbl_x, lbl_y);
+
+
+}
+
+void MainWindow::SetFolder(const QString &d)
+{
+    current_folder_ = d;
+    cur_file_index_ = 0;
+    SetPixmap(GetCurrentFile());
+}
+
+QString MainWindow::GetCurrentFile()
+{
+    QDir dir(current_folder_);
+    QStringList list = dir.entryList();
+
+
+    // Сохраним количество файлов.
+    int total = int(list.size());
+
+    int file_index = std::min(cur_file_index_, total - 1);
+    file_index = std::max(0, file_index);
+
+    // Используем file_index, а не cur_file_index_.
+    return dir.filePath(list[file_index]);
+}
+
+void MainWindow::resizeEvent(QResizeEvent*)
+{
+    FitImage();
+}
+
+
--- problems/sprint2/photoalbum-3/submission/mainwindow.h
+++ problems/sprint2/photoalbum-3/submission/mainwindow.h
@@ -1,2 +1,47 @@
 // Возьмите решение предыдущей задачи
 // о фотоальбоме в качестве заготовки.
+
+// Решение предыдущей задачи
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QDir>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void SetPixmap(const QString path) {
+        active_pixmap = QPixmap(path);
+    }
+
+    void FitImage();
+    void SetFolder(const QString& d);
+    QString GetCurrentFile();
+
+
+private:
+    void resizeEvent(QResizeEvent *event) override;
+
+
+private:
+    Ui::MainWindow *ui;
+    QLabel lbl_new_{this};
+    QPixmap active_pixmap;
+    QString current_folder_;
+    int cur_file_index_ = 0;
+};
+#endif // MAINWINDOW_H
