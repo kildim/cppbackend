57c1df34f918d1c4d18e45b2d50159a9226f1f5f
Dmitry Kilin <73240204+kildim@users.noreply.github.com>
2024-12-21 21:05:16 +0300

    feat: s2-12-9-1 alarm work but tests dont passes

--- problems/sprint2/alarm/submission/mainwindow.cpp
+++ problems/sprint2/alarm/submission/mainwindow.cpp
@@ -9,9 +9,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    player_.setAudioOutput(&audio_output_);
+    audio_output_.setVolume(100);
 
+    timer_one_sec_.setSingleShot(true);
     connect(&timer_one_sec_, &prac::QTimer::timeout, this, &MainWindow::on_timer_timeout);
     timer_one_sec_.start(1000);
+
 
 }
 
@@ -38,38 +42,53 @@ void MainWindow::update_timeout_indicator()
         alarm,
         now = prac::QDateTime::currentDateTime();
 
+    int
+        hours,
+        minutes,
+        seconds,
+        seconds_attaining;
+
     if (!is_alarm_on) {
         ui->lbl_timeout->setText("Установите будильник");
         return;
     };
 
-    alarm = now;
-    alarm.setTime(QTime(
-        ui->sb_hour->text().toInt(),
-        ui->sb_min->text().toInt(),
-        ui->sb_sec->text().toInt()
-        ));
-    if (alarm < now) {
-        alarm = alarm.addDays(1);
-    };
+    if (!is_playing_alarm_) {
+        alarm = now;
+        alarm.setTime(QTime(
+            ui->sb_hour->text().toInt(),
+            ui->sb_min->text().toInt(),
+            ui->sb_sec->text().toInt()
+            ));
+        if (alarm < now) {
+            alarm = alarm.addDays(1);
+        };
 
-    int
-        hours,
-        minutes,
-        seconds,
         seconds_attaining = ((alarm - now).count() + 500) / 1000;
 
-    hours = seconds_attaining/3600;
-    minutes = seconds_attaining%3600/60;
-    seconds = seconds_attaining%3600%60;
+        if (seconds_attaining == 86400) seconds_attaining = 0;
 
 
-    ui->lbl_timeout->setText(
-        QString("%1:%2:%3")
-            .arg(hours, 2, 10, QChar ('0'))
-            .arg(minutes, 2, 10, QChar ('0'))
-            .arg(seconds, 2, 10, QChar ('0'))
+        hours = seconds_attaining/3600;
+        minutes = seconds_attaining%3600/60;
+        seconds = seconds_attaining%3600%60;
+
+        ui->lbl_timeout->setText(
+            QString("%1:%2:%3")
+                .arg(hours, 2, 10, QChar ('0'))
+                .arg(minutes, 2, 10, QChar ('0'))
+                .arg(seconds, 2, 10, QChar ('0'))
         );
+
+        if (seconds_attaining == 0) {
+            is_playing_alarm_ = true;
+            ui->lbl_timeout->setText("Пора вставать");
+            if (!file_name_.isEmpty()) {
+                player_.setSource(QUrl::fromLocalFile(file_name_));
+                player_.play();
+            }
+        }
+    }
 }
 
 void MainWindow::on_timer_timeout()
@@ -84,7 +103,7 @@ void MainWindow::on_timer_timeout()
     update_timeout_indicator();
 
     estimated_timeout = 1000 - now.msec();
-    timer_one_sec_.stop();
+    //timer_one_sec_.stop();
     timer_one_sec_.start(estimated_timeout);
 }
 
@@ -96,6 +115,25 @@ void MainWindow::on_pb_start_stop_clicked()
     button_text = is_alarm_on ? QString("Старт") : QString("Стоп");
     ui->pb_start_stop->setText(button_text);
     is_alarm_on = !is_alarm_on;
+
+    if (is_playing_alarm_) {
+        player_.stop();
+        is_playing_alarm_ = false;
+    };
+
     update_timeout_indicator();
+}
+
+
+
+void MainWindow::on_action_load_file_triggered()
+{
+    file_name_ = prac::QFileDialog::getOpenFileName(this,
+                                                           QString::fromUtf8("Открыть файл"),
+                                                           QDir::currentPath(),
+                                                           "*.wav;*.mp3");
+    if (!file_name_.isEmpty()) {
+        ui->lbl_melody->setText(file_name_);
+    }
 }
 
--- problems/sprint2/alarm/submission/mainwindow.h
+++ problems/sprint2/alarm/submission/mainwindow.h
@@ -24,11 +24,15 @@ private:
     Ui::MainWindow *ui;
     prac::QTimer timer_one_sec_{this};
     prac::QMediaPlayer player_{this};
+    QAudioOutput audio_output_{this};
     bool is_alarm_on = false;
+    bool is_playing_alarm_=false;
+    QString file_name_;
 
     void update_time_indicator();
     void update_timeout_indicator();
 private slots:
     void on_timer_timeout ();
     void on_pb_start_stop_clicked();
+    void on_action_load_file_triggered();
 };
